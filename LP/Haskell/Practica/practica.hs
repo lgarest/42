aa284b58a12b04d617e991036272eed7e69febeb
lgarest <luisgarciaestrades@gmail.com>
2014-11-30 00:38:39 +0100

    LP: RString finalizado

--- LP/Haskell/Practica/practica.hs
+++ LP/Haskell/Practica/practica.hs
@@ -32,6 +32,11 @@ class Rewrite a where
 {- 3. Rule & RewriteSystem --------------------------------------------------}
 
 {- 3.1 -}
+--noSubs :: Rewrite a => [(Position, Substitution a)] -> [(Position, a)]
+noSubs :: [(t1, Substitution t)] -> [(t1, [(t, t)])]
+noSubs [] = []
+noSubs ((p, Substitution s):xs) = [(p,s)] ++ noSubs xs
+
 data Rule a = Rule a a
 
 data RewriteSystem a = RewriteSystem[Rule a]
@@ -50,10 +55,50 @@ validRule sign (Rule a b) = (valid sign a) && (valid sign b)
 validRewriteSystem :: Rewrite a => Signature -> RewriteSystem a -> Bool
 validRewriteSystem sign (RewriteSystem rs) = and $ map (validRule sign) rs
 
+
 {- 3.2 -}
 type Strategy a = [(Position, a)] -> [(Position, a)]
 
 {- 3.3 -}
+oneStepRewrite :: Rewrite a => RewriteSystem a -> a -> Strategy a -> a
+oneStepRewrite (RewriteSystem sys) s strategy
+    | not $ matchSomething (RewriteSystem sys) s = s
+    | otherwise = foldl(\x y -> replace x [y]) s matches
+    where matches = strategy $ (getMatches (RewriteSystem sys) s)
+
+rewrite :: Rewrite a => RewriteSystem a -> a -> Strategy a -> a
+rewrite rws s stgy
+    | not $ matchSomething rws s = s
+    | otherwise = rewrite rws (evaluate (oneStepRewrite rws s stgy)) stgy
+
+nrewrite :: Rewrite a => RewriteSystem a -> a -> Strategy a -> Int -> a
+nrewrite rws s stgy n
+    | n == 0 = s
+    | not $ matchSomething rws s = s
+    | otherwise = nrewrite rws (evaluate (oneStepRewrite rws s stgy)) stgy (n-1)
+
+-- applyOnPos a n m b
+
+--[(Position, Substitution RString)]
+
+--match str1 str2
+getMatches :: Rewrite a => RewriteSystem a -> a -> [(Position, a)]
+getMatches (RewriteSystem []) _ = []
+getMatches (RewriteSystem ((Rule a b):rules)) str = smatch ++ getMatches (RewriteSystem rules) str
+    where smatch = (map(\(x,Substitution y) -> (x,b)) (match a str))
+
+sort :: Ord a => [(a, b)] -> [(a, b)]
+sort [] = []
+sort [x] = [x]
+sort l@(x:xs) = sort [b | b <- xs, fst b <= fst x] ++ [x] ++
+    sort [b | b <- xs, fst b > fst x]
+
+-- devuelve si alguna regla hace matching con el string
+matchSomething :: Rewrite a => RewriteSystem a -> a -> Bool
+matchSomething (RewriteSystem []) _ = False
+matchSomething (RewriteSystem ((Rule a b):rules)) str
+    | length (match a str) /= 0 = True
+    | otherwise = matchSomething (RewriteSystem (rules)) str
 
 {- 4. RString ---------------------------------------------------------------}
 
@@ -89,7 +134,7 @@ data RString = RString String deriving(Eq)
 
 instance Rewrite (RString) where
     getVars _ = []
-    -- valida un RString: definido por la signatura y bien formado
+
     valid signature str = (wellComposed str) && (strInSignat signature str)
 
     match str1 str2 = matchC str1 str2 0
@@ -98,9 +143,9 @@ instance Rewrite (RString) where
 
     replace str lista = foldl (\x y ->
         if (length $ fst y) == 1 then
-            applyOnPos str (head $ fst y) (length' $ snd y) (snd y)
+        applyOnPos str (head $ fst y) (length' $ snd y) (snd y)
         else
-            applyOnPos str (head $ fst y) ((last $ fst y)-(head $ fst y)) (snd y)
+        applyOnPos str (head $ fst y) ((last $ fst y)-(head $ fst y)) (snd y)
         ) str lista
 
     evaluate str = str
@@ -117,20 +162,20 @@ strInSignat sign str = and $ map (symbolInSignat sign) (getSymbols str)
 symbolInSignat :: Signature -> RString -> Bool
 symbolInSignat sign str = elem (show str) (map fst sign)
 
-
+-- itera sobre a y comprueba si el str hace matching
 matchC :: RString -> RString -> Int -> [(Position, Substitution a)]
 matchC str a n
     | length' a == 0 = []
     | take' (length' str) a == str = [([n], Substitution [])] ++ rec
     | otherwise = matchC str (tail' a) (n+1)
-    where rec = matchC str (drop' (length' str) a) (n+1)
+    where rec = matchC str (drop' (length' str) a) (n+(length' str))
 
---applyChange s (a,b) = foldl(\s y m -> applyOnPos s (head $ fst y) m b)
---    s (match a s) (length' a)
---(RString, RString)
+-- aplica el cambio del snd donde haga matching el fst en el primer parámetro
 applyChange :: RString -> (RString,RString) -> RString
-applyChange s (a,b) = foldl(\x y -> applyOnPos x (head $ fst y) m b) s (match a s)
+applyChange s (a,b) = foldl(\x y ->
+    applyOnPos x (head $ fst y) m b) s (match a s)
     where m = length' a
+
 -- inyecta en la posicion n (comiendo m caracteres) de a el string b
 applyOnPos :: RString -> Int -> Int -> RString -> RString
 applyOnPos a n m b = concat' (concat' prefix b) sufix
@@ -155,10 +200,6 @@ getSymbols str
     where symbol = getSymbol str
           next = drop' (length' symbol) str
 
-{- UNUSED -}
-rStrTChar :: RString -> [Char]
-rStrTChar (RString str) = str
-
 {- 4.2 -}
 readRString :: String -> RString
 readRString str = RString str
@@ -180,8 +221,17 @@ instance Show (RString) where
     show(RString a) = init(tail(show a))
 
 {- 4.4 -}
-leftmost a = a {- TODO -}
-rightmost b = b {- TODO -}
+-- [(Position, a)]
+leftmost :: Strategy (RString)
+leftmost [] = []
+leftmost l = [head $ sort l]
+rightmost :: Strategy (RString)
+rightmost [] = []
+rightmost l = [last $ sort l]
+--parallelinnermost _ = []
+--leftmostinnermost _ = []
+--paralleloutermost _ = []
+--leftmostoutermost _ = []
 
 {- 5. RTerm -----------------------------------------------------------------}
 
